2cdd92b71efdec212e3bb4408acc1e3f94de329d
prelest19 <borskoe1908@icloud.com>
2026-03-03 22:37:41 +0400

    Update verify.py

--- data/verify/verify.py
+++ data/verify/verify.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+def read_matrix(filename):
+    return np.loadtxt(filename)
+
+A = read_matrix("../data/matrixA.txt")
+B = read_matrix("../data/matrixB.txt")
+C_cpp = read_matrix("../data/resultMatrix.txt")
+
+C_python = np.dot(A, B)
+
+if np.allclose(C_cpp, C_python):
+    print("Verification SUCCESS: Results match.")
+else:
+    print("Verification FAILED: Results do not match.")
